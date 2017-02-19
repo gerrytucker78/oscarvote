d1990ea6c09219d1d5d956acf4f0b4a5770fade1
Gerry Tucker <gerrytucker@gmail.com>
2017-02-19 13:04:42 +0000

    Abstracted password out to a configuration file for security purposes.  Added new startup scripts

--- models/db.js
+++ models/db.js
@@ -1,6 +1,19 @@
-// models/awards.js
+var config = require('config');
+
+var dbConfig = config.get('db');
+
+
 var db = require("mongoose");
 db.Promise = global.Promise;
-db.connect("mongodb://localhost/oscars");
+
+var dbUrl = "mongodb://";
+
+if (dbConfig.get("authRequired") == "true") {
+  dbUrl = dbUrl + dbConfig.get("user") + ":" + dbConfig.get("password") + "@";
+}
+
+dbUrl = dbUrl + dbConfig.get("host") + "/" + dbConfig.get("db");
+
+db.connect(dbUrl);
 
 module.exports = db;
