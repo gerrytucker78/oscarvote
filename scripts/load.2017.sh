4ab393390bbcad92f05864864a2ede1903b63388
Gerry Tucker <gerrytucker@gmail.com>
2017-02-19 00:46:36 -0600

    Added one other category for testing purposes

--- scripts/load.2017.sh
+++ scripts/load.2017.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+## Actor in a Leading Role
+curl http://localhost:3000/candidates/add/2017.Actor%20in%20a%20Leading%20Role.Casey%20Affleck%20%28Manchester%20By%20The%20Sea%29
+curl http://localhost:3000/candidates/add/2017.Actor%20in%20a%20Leading%20Role.Andrew%20Garfield%20%28Hacksaw%20Ridge%29
+curl http://localhost:3000/candidates/add/2017.Actor%20in%20a%20Leading%20Role.Ryan%20Gossling%20%28La%20La%20Land%29
+curl http://localhost:3000/candidates/add/2017.Actor%20in%20a%20Leading%20Role.Viggo%20Mortensen%20%28Captain%20Fantastic%29
+curl http://localhost:3000/candidates/add/2017.Actor%20in%20a%20Leading%20Role.Denzel%20Washington%20%28Fences%29
+
 ## Best Picture
 curl http://localhost:3000/candidates/add/2017.Best%20Picture.Arrival
 curl http://localhost:3000/candidates/add/2017.Best%20Picture.Fences
